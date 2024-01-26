b3dc124401d347a2db4fe8a9fe26394357a16d2e
martinaragusa <885113@stud.unive.it>
2024-01-26 17:15:56 +0100

    Aggiunta schermata per alimenti in scadenza e scaduti

---
+++ app/src/main/java/com/example/nowaste/ListaAlimentiScaduti.java
@@ -0,0 +1,14 @@
+package com.example.nowaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaAlimentiScaduti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alimenti_scaduti);
+    }
+}

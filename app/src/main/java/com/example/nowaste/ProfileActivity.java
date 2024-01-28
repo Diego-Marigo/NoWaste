a2f1e678bfc9f7c517678dbbecf6da24987fa6a0
martinaragusa <885113@stud.unive.it>
2024-01-28 21:29:36 +0100

    Modificato pagina del profilo (aggiunte statistiche)

--- app/src/main/java/com/example/nowaste/ProfileActivity.java
+++ app/src/main/java/com/example/nowaste/ProfileActivity.java
@@ -19,8 +19,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * ProfileActivity:
@@ -34,13 +42,12 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
     /**
      * Elementi UI della pagina e user attualmente autenticato.
      */
-    private TextView mUsername;
-    private TextView mEmail;
-    private ImageView profilePic;
+    private TextView mEmail, alimentoSprecato, stats;
     private BottomNavigationView bottomNavigationView;
     FirebaseUser user;
     private ImageButton settingsBtn, alertBtn, listsBtn;
     private DatabaseReference mDatabase;
+    Map<String, Integer> statistiche;
 
     /**
      * Metodo onCreate chiamato all'avvio dell'activity.
@@ -52,15 +59,15 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mUsername = findViewById(R.id.username);
         mEmail = findViewById(R.id.email);
-        profilePic = findViewById(R.id.profile_picture);
         user = FirebaseAuth.getInstance().getCurrentUser();
         settingsBtn = findViewById(R.id.settings_icon);
         alertBtn = findViewById(R.id.alert_icon);
         listsBtn = findViewById(R.id.lists_icon);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.profile);
+        alimentoSprecato = findViewById(R.id.alimentoSprecato);
+        stats = findViewById(R.id.stats);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             if(item.getItemId() == R.id.home){
@@ -72,7 +79,6 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
         });
 
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
-        mUsername.setText(user.getDisplayName());
         mEmail.setText(user.getEmail());
 
         settingsBtn.setOnClickListener(new View.OnClickListener() {
@@ -104,6 +110,50 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
                 finish();
             }
         });
+
+        // setto il testo dell'alimento più sprecato
+        // mi devo recuperare gli alimenti nella lista degli alimenti scaduti
+        List<Alimenti> alimentiScaduti = new ArrayList<>();
+        mDatabase.child("Alimenti").child(user.getUid());
+        // TODO da decommentare ma la lista per ora è vuota quindi va in eccezione
+        /*mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Alimenti a = snapshot.getValue(Alimenti.class);
+                if(Integer.parseInt(a.listId) == 1) {
+                    alimentiScaduti.add(a);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        // conto le occorrenze di ciascun alimento
+        Map<Alimenti, Long> occorrenze = alimentiScaduti.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        // trovo l'alimento con più occorrenze
+        Alimenti alSprecato = Collections.max(occorrenze.entrySet(), Map.Entry.comparingByValue()).getKey();
+        alimentoSprecato.setText(alSprecato.nomeAlimento);
+*/
+        // popolo la mappa con i dati di Waste Watcher Italia
+        /*statistiche.put("Frutta", 25);
+        statistiche.put("Insalata", 18);
+        statistiche.put("Cipolla", 17);
+        statistiche.put("Patate", 17);
+        statistiche.put("Pane", 16);
+        statistiche.put("Verdure", 16);
+
+        int stat = 0;
+        for(Alimenti a : alimentiScaduti) {
+            for(String key : statistiche.keySet()) {
+                if (a.nomeAlimento.equals(key)) {
+                    stat++;
+                }
+            }
+        }
+        // TODO ho messo un valore a caso per ora
+        stats.setText(stat * 20);*/
     }
 
     /**
